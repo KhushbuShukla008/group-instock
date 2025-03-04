d81187bd175c1c26d425d13b39017891ef2b3cb0
“Mimta <mimta.chowdhury@gmail.com>
2025-03-04 17:01:01 -0500

    add placeholder for HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
         {/* placeholder for paths */}
         {/* <Route path="/inventory" element={<Inventory />} />
         <Route path="/warehouses" element={<Warehouses />} /> */}
